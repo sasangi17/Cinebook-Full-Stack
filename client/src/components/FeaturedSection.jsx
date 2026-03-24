268c18fec696bf9a7dbc4d9d74d6ba5bdca496f5
29849 <135608399+sasangi17@users.noreply.github.com>
2026-03-24 23:13:38 +0530

    Remove the BlurCicle in featured Section

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,7 +1,6 @@
 import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import BlurCircle from './BlurCircle'
 import MovieCard from './MovieCard'
 import dummyShowsData from "../assets/dummyShowsData";
 
@@ -11,7 +10,7 @@ const FeaturedSection = () => {
   return (
    <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
      <div className='flex items-center justify-between pt-20 pb-10'>
-        <BlurCircle top='0' right='-80px'/>
+        
          <p className='text-xl font-semibold text-white'>Now Showing</p> <button onClick={() => navigate('/movies')} className='group flex items-center gap-2 text-sm text-gray-300 hover:text-white transition cursor-pointer'>View All
          <ArrowRight className='w-4 h-4 group-hover:translate-x-1 transition-transform'/></button></div> 
     <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
